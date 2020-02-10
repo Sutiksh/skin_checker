e0f80642ab5f9ea2c9acb4cd490fc1e1228b0e4b
Sutiksh Verma <verma.sutiksh27@gmail.com>
2020-02-10 23:05:36 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:skin_checker/pages/home_screen.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: 'SKin Checker',
+    home: HomeScreen(),
+  ));
+}
