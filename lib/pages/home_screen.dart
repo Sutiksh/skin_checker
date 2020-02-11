d6221f65a01c6e2acedcd8c571b6e4d5f5c99a9d
Sutiksh Verma <verma.sutiksh27@gmail.com>
2020-02-11 12:12:20 +0530

    Redesigned the ui And changed the image picking module

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:skin_checker/pages/take_picture_screen.dart';
 import 'package:skin_checker/pages/pick_image.dart';
-
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,46 +13,119 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  File _imageFile;
 
+  // To track the file uploading state
+  bool _isUploading = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: 300,
-          height: 300,
-          color: Colors.redAccent,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+  void _openImagePickerModal(BuildContext context) {
+    final flatButtonColor = Theme.of(context).primaryColor;
+    print('Image Picker Modal Called');
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 150.0,
+            padding: EdgeInsets.all(10.0),
             child: Column(
               children: <Widget>[
-                RaisedButton(
-                  onPressed: (){
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => PickImage(),
-                      )
-                    );
-                  },
-                  child: Text("Browse From Device"),
+                Text(
+                  'Pick an image',
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                RaisedButton(
-                  onPressed: () async {
-                    final cameras = await availableCameras();
-                    final firstCamera = cameras.first;
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => TakePictureScreen(
-                        camera: firstCamera,
-                      )),
-                    );
+                SizedBox(
+                  height: 10.0,
+                ),
+                FlatButton(
+                  textColor: flatButtonColor,
+                  child: Text('Use Camera',style: TextStyle(color: Colors.white),),
+                  onPressed: () {
+                    _getImage(context, ImageSource.camera);
                   },
-                  child: Text("Open Camera"),
-                )
+                ),
+                FlatButton(
+                  textColor: flatButtonColor,
+                  child: Text('Use Gallery',style: TextStyle(color: Colors.white),),
+                  onPressed: () {
+                    _getImage(context, ImageSource.gallery);
+                  },
+                ),
               ],
             ),
-          ),
+          );
+        });
+  }
+
+  Widget _buildUploadBtn() {
+    Widget btnWidget = Container();
+    if (_isUploading) {
+      // File is being uploaded then show a progress indicator
+      btnWidget = Container(
+          margin: EdgeInsets.only(top: 10.0),
+          child: CircularProgressIndicator());
+    } else if (!_isUploading && _imageFile != null) {
+      // If image is picked by the user then show a upload btn
+      btnWidget = Container(
+        margin: EdgeInsets.only(top: 10.0),
+        child: RaisedButton(
+          child: Text('Upload'),
+          onPressed: () {
+//            _startUploading();
+          },
+          color: Colors.pinkAccent,
+          textColor: Colors.white,
         ),
+      );
+    }
+    return btnWidget;
+  }
+
+  void _getImage(BuildContext context, ImageSource source) async {
+    File image = await ImagePicker.pickImage(source: source);
+    setState(() {
+      _imageFile = image;
+    });
+    // Closes the bottom sheet
+    Navigator.pop(context);
+  }
+
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Upload Demo'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0, left: 10.0, right: 10.0),
+            child: OutlineButton(
+              onPressed: () => _openImagePickerModal(context),
+              borderSide:
+                  BorderSide(color: Theme.of(context).accentColor, width: 1.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.camera_alt),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  Text('Add Image'),
+                ],
+              ),
+            ),
+          ),
+          _imageFile == null
+              ? Text('Please pick an image')
+              : Image.file(
+                  _imageFile,
+                  fit: BoxFit.cover,
+                  height: 300.0,
+                  alignment: Alignment.topCenter,
+                  width: MediaQuery.of(context).size.width,
+                ),
+          _buildUploadBtn(),
+        ],
       ),
     );
   }
